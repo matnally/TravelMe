d465bf6faac92b042c7732e92ba9e48552e37b37
MatNally <matthew@hotmail.com>
2017-09-20 16:40:23 +0100

    Some updates

---
+++ _json/me.js
@@ -0,0 +1,11 @@
+
+var JSONme = { "me" : [
+  {
+    "name"      : "Me"
+    ,"holidays" : 50
+    ,"daysLeft" : 50
+    ,"distanceTravelled" : 50
+    ,"locationPrevious" : 0
+    ,"locationCurrent" : 0 //index of JSONdestinations
+  }
+]};
