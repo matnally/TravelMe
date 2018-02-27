ceb4632ffef0babd6f131fbb900279510f4732f7
matnally <matnally@users.noreply.github.com>
2018-02-27 15:50:23 +0000

    Added bubble fading on selected map bubbles!

--- _js/map.js
+++ _js/map.js
@@ -79,16 +79,16 @@ function createMap() {
   });
   //END COUNTRY CLICK
 
-
-
-
 //START BUBBLE CLICK
 map.svg.selectAll('.datamaps-bubble').on('click', function(e,data) {
-//  alert("bubble click");
 
+  //apply fade style to chosenDestination
+  makeBubbleSelectedFade(e.name);
+
+  //zoom in on chosenDestination???
   map.svg.call(d3.behavior.zoom().on("zoom", redraw));
-  //console.log(e.name);
-  //console.log(data);
+
+  //apply action function
   actionSelect('flight', data);
 
   if (JSONlocation[0].locationCurrent != data) {
@@ -167,70 +167,6 @@ var intIClickedOn=0;
 
 
 
-
-
-
-
-
-function updateMap() {
-
-  var mapObject = $("#jvectormap").vectorMap("get", "mapObject");
-      mapObject.clearSelectedMarkers();
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("fill", "RED");
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("stroke", "RED");
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("r", 5);
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("fill", "blue");
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("stroke", "#000");
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("r", 10);
-
-}
-
-
-function updateMapCurrentLocation() {
-
-  var mapObject = $("#jvectormap").vectorMap("get", "mapObject");
-//      mapObject.clearSelectedMarkers(); //IF USE, NO RECORD OF WHERE FLOWN!
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("fill", "RED");
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("stroke", "RED");
-      mapObject.markers[JSONlocation[0].locationPrevious].element.setStyle("r", 5);
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("fill", "blue");
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("stroke", "#000");
-      mapObject.markers[JSONlocation[0].locationCurrent].element.setStyle("r", 10);
-
-}
-
-
-
-function onMarkerOutUpdateMap() {
-  //get selected marker
-  var mapObject = $("#jvectormap").vectorMap("get", "mapObject");
-
-  //check if something is actually selected
-  if (mapObject.getSelectedMarkers() != "") {
-    travelObjectUpdate(parseInt(mapObject.getSelectedMarkers())); //display destination details
-    travelObjectDisplay(parseInt(mapObject.getSelectedMarkers())); //display destination details
-    travelButtonShowHide(mapObject.getSelectedMarkers());
-
-    //TODO
-    if (JSONlocation[0].locationCurrent == mapObject.getSelectedMarkers()) {
-      //same location as current!
-      updateTurnDetails(""); //???
-    } else {
-      updateTurnDetails(travelGetTravelDetails()); //???
-    }
-
-  } else {
-    travelObjectUpdate(parseInt(JSONlocation[0].locationCurrent)); //display destination details
-    travelObjectDisplay(parseInt(JSONlocation[0].locationCurrent)); //display destination details
-    travelButtonShowHide(JSONlocation[0].locationCurrent);
-    updateTurnDetails(""); //???
-  } //if
-
-}//function
-
-
-
-
 function getRegionDetails(strRegion) {
 
 var strTemp = "";
@@ -297,5 +233,33 @@ function updateMap() {
     //END MAP UPDATE
 
   } //if
+
+} //function
+
+
+
+function makeBubbleSelectedFade(strBubbleSelected) {
+
+  // add the fading bubbles plugin
+  map.addPlugin('fadingBubbles', fadingBubbles);
+  // fetch the data
+  d3.json(JSONdestinations, function(error) {
+      if (error) return console.warn(error);
+      drawBubbles(JSONdestinations);
+      var sleep = (JSONdestinations.length - 1) * 100;
+      setInterval(function() {
+          drawBubbles(JSONdestinations);
+      }, sleep);
+  });
+  // draw the fading bubbles at staggered intervals
+  drawBubbles = function(data) {
+      data.forEach(function(datum, index) {
+        if (datum.name == strBubbleSelected) { //get selected bubble
+          setTimeout(function() {
+              map.fadingBubbles([datum]);
+          }, index * 0.1); //setTimeout(function() {
+        } //if
+      }); //data.forEach(function(datum, index) {
+  } //drawBubbles = function(data) {
 
 } //function
