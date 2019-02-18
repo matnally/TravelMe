78f19d1d3a2e779f23ba259f598d275b75467305
Matthew McNally <matthew.mcnally@digital.hmrc.gov.uk>
2019-02-18 09:15:38 +0000

    Added missing Result js

---
+++ _js/result.js
@@ -0,0 +1,24 @@
+
+function resGetResult() {
+
+  var intMoney = 0;
+  var intHappiness = 0;
+  var intDestinations = 0;
+  var intLuxuries = 0;
+  var intDaysWorked = 0;
+  var intDistanceTravelled = 0;
+  var intJob = 0;
+
+  intMoney = JSONplayer[0].money
+  intHappiness = JSONplayer[0].happiness
+  intDestinations = JSONplayer[0].destination.length
+  intLuxuries = JSONplayer[0].luxury.length
+  intDaysWorked = JSONplayer[0].daysWorked
+  intDistanceTravelled =JSONplayer[0].distanceTravelled
+  intJob = JSONplayer[0].job
+
+  //WORLD WONDER BONUS? Continent checklist
+
+  return "Dunno";
+
+} //function
---
+++ _json/result.js
@@ -0,0 +1,11 @@
+
+var JSONresult = [
+  {
+    "name"          : "Money Grabber"
+    ,"description"  : "You are a Money Grabber"
+  }
+  ,{
+    "name"          : "World Traveller"
+    ,"description"  : "You are a World Traveller"
+  }
+];
