04dc9cd9cee87058adb8860c23e77ef237f777ea
Matthew McNally <matthew.mcnally@digital.hmrc.gov.uk>
2019-02-21 15:05:22 +0000

    More results

--- _js/result.js
+++ _js/result.js
@@ -25,11 +25,32 @@ function resShowResult() {
 //   console.log("Most visited: " + JSONdestination[arrTemp[i]].name);
 // } //for
 
+//WORK
+defUpdateElement("spnWorkTotalDays", defThousandsDelimiter(JSONplayer[0].daysWorked) + " ");
+var intWorkTotalPercentage = 0;
+intWorkTotalPercentage = defCalcPercentage(JSONplayer[0].daysWorked, JSONgame[0].days).toFixed(0);
+defUpdateElement("spnWorkTotalPercentage", intWorkTotalPercentage + "%");
+defUpdateElement("spnWorkPromoted", JSONplayer[0].job);
+var intWorkPromotedPercentage = 0;
+intWorkPromotedPercentage = defCalcPercentage(JSONplayer[0].job, JSONjob[0].length).toFixed(0);
+defUpdateElement("spnWorkPromotedPercentage", intWorkTotalPercentage + "%");
+defUpdateElement("spnWorkTotalHappiness", defThousandsDelimiter(JSONplayer[0].daysWorked * JSONgame[0].workHappiness));
+
 //TRAVEL
 var intTravelTotalPercentage = 0;
 intTravelTotalPercentage = defCalcPercentage(JSONplayer[0].daysTravelled, JSONgame[0].days).toFixed(0);
 defUpdateElement("spnTravelTotalPercentage", intTravelTotalPercentage + "%");
 defUpdateElement("spnTravelTotalDistance", defThousandsDelimiter(JSONplayer[0].distanceTravelled) + " " + JSONgame[0].measure);
+var intTravelTotalHappiness = 0;
+for (d in JSONplayer[0].destination) {
+  intTravelTotalHappiness = (intTravelTotalHappiness + JSONdestination[JSONplayer[0].destination[d]].happiness);
+} //for
+defUpdateElement("spnTravelTotalHappiness", defThousandsDelimiter(intTravelTotalHappiness));
+var intTravelTotalSpent = 0;
+for (d in JSONplayer[0].destination) {
+  intTravelTotalSpent = (intTravelTotalSpent + JSONdestination[JSONplayer[0].destination[d]].cost);
+} //for
+defUpdateElement("spnTravelTotalSpent", JSONgame[0].currency + defThousandsDelimiter(intTravelTotalSpent));
 
 //DESTINATIONS
 var arrDestinationVisitedUnique = [];
@@ -75,8 +96,39 @@ defUpdateElement("spnCountryTotal", intCountries.length);
 defUpdateElement("spnCountryVisitedPercentage", defCalcPercentage(intCountriesCompleted, intCountries.length).toFixed(0) + "%");
 
 
-
 defUpdateElement("spnCountryVisitedMost", "???");
+
+
+
+//LUXURIES
+var intLuxuryTotalSpend = 0;
+for (l in JSONplayer[0].luxury) {
+  intLuxuryTotalSpend = (intLuxuryTotalSpend + JSONluxury[JSONplayer[0].luxury[l]].cost);
+} //for
+defUpdateElement("spnLuxuryTotalSpend", JSONgame[0].currency + defThousandsDelimiter(intLuxuryTotalSpend));
+var intLuxuryTotalHappiness = 0;
+for (l in JSONplayer[0].luxury) {
+  intLuxuryTotalHappiness = (intLuxuryTotalHappiness + JSONluxury[JSONplayer[0].luxury[l]].happiness);
+} //for
+defUpdateElement("spnLuxuryTotalHappiness", JSONgame[0].currency + defThousandsDelimiter(intLuxuryTotalHappiness));
+
+//PLAYER
+var strWorkTravelPreference = "";
+switch (true) {
+  case (JSONplayer[0].daysTravelled  > JSONplayer[0].daysWorked):
+    strWorkTravelPreference = "Travelling";
+  break;
+  case (JSONplayer[0].daysWorked  > JSONplayer[0].daysTravelled):
+    strWorkTravelPreference = "Working";
+  break;
+  case (JSONplayer[0].daysWorked  === JSONplayer[0].daysTravelled):
+    strWorkTravelPreference = "Same!";
+  break;
+  default:
+    strWorkTravelPreference = "Dunno";
+} //switch
+defUpdateElement("spnPlayerLifeBalance", strWorkTravelPreference);
+
 
 
 
